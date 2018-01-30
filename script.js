8bfe24a6cbf35aaec7f4b8208bf2daedf4e71779
Reddyloka <anvesh.reddy@venturepact.com>
2018-01-30 10:33:12 +0530

    finished

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function myfun()
 {
-   fetch("https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=94e25a4c66eb4ea791f3898bdce7bcea")
-    .then(data => data.json())
-    .then(data =>{
-     for(let i=0;i<10;i++)
+ fetch("https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=94e25a4c66eb4ea791f3898bdce7bcea")
+  .then(data => data.json())
+     .then(data =>{
+      for(let i=0;i<10;i++)
        {
          document.getElementById("headline"+i).innerHTML=data.articles[i].title;
           document.getElementById("li"+i).href=data.articles[i].url;
@@ -14,11 +14,10 @@ function myfun()
  .catch((err)=> {console.error(err);})
 }
 //different channels
- var len= document.getElementsByClassName('ft');
- var arr=len.length;
- for(let i=0;i<arr;i++){
-	
-   len[i].addEventListener("click",function(){
+ var m= document.getElementsByClassName('ft');
+ var z=m.length;
+ for(let i=0;i<z;i++){
+   m[i].addEventListener("click",function(){
      channels(this.id);
    });
  }
@@ -35,46 +34,45 @@ function channels(channelname)
     }})
   .catch((err)=> {console.error(err);})
 }
-
 //filter
 function sorting()
 {
 
 let x=document.getElementById("input").value;
  var search=[];
- for(let i=0;i<8;i++)
+ for(let k=0;k<8;k++)
  {
-
-   if(document.getElementById("ch"+i).checked)
-   { let v1=document.getElementById("ch"+i).value;
+   if(document.getElementById("ch"+k).checked)
+   { let v1=document.getElementById("ch"+k).value;
      search.push(v1);
    }
  }
  if(x.length>0)
  {
-   if(search.length >0)
-    {
-       fetch(`https://newsapi.org/v2/everything?q=${x}&sources=${search}&apiKey=94e25a4c66eb4ea791f3898bdce7bcea`)
-       .then(data => data.json())
+ if(search.length >0)
+ {
+   fetch(`https://newsapi.org/v2/everything?q=${x}&sources=${search}&apiKey=94e25a4c66eb4ea791f3898bdce7bcea`)
+    .then(data => data.json())
        .then(data =>{
-     if(data.articles == "")
+         if(data.articles == "")
          {
               alert("News Not Available in "+search);
          }
-     else {
+         else{
         for(let i=0;i<10;i++)
          {document.getElementById("headline"+i).innerHTML=data.articles[i].title;
          document.getElementById("li"+i).href=data.articles[i].url;
          document.getElementById('image'+i).src =data.articles[i].urlToImage;
         document.getElementById("de"+i).innerHTML=data.articles[i].description;}
          }})
-    }
-    else
-     {alert("Please Select Atleast One Channel");
-     }
- }
- else
-    {
-	alert("Please Enter any KeyWord to Search");
-	}
+}
+else
+  {
+    alert("Please Select Atleast One Channel");
+  }
+}
+else
+  {
+    alert("Please Enter any KeyWord to Search");
+  }
 }
