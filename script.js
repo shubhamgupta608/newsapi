ba1d5d743512b1da48f03b985778d215aa3c7f0e
Reddyloka <anvesh.reddy@venturepact.com>
2018-01-30 23:03:05 +0530

    modified

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
+
 function myfun()
 {
  fetch("https://newsapi.org/v2/top-headlines?sources=google-news-in&apiKey=94e25a4c66eb4ea791f3898bdce7bcea")
   .then(data => data.json())
      .then(data =>{
-      for(let i=0;i<10;i++)
+      for(let i=0;i<data.articles.length;i++)
        {
-         document.getElementById("headline"+i).innerHTML=data.articles[i].title;
-          document.getElementById("li"+i).href=data.articles[i].url;
-         document.getElementById('image'+i).src=data.articles[i].urlToImage;
-         document.getElementById("de"+i).innerHTML=data.articles[i].description;
+         if(i<3)
+         {
+         var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+         <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+           <p id="de">${data.articles[i].description}</p>`;
+
+       var l= document.createElement('div');
+           // l.setAttribute("id","main-headline")
+             l.innerHTML=t;
+           var h=document.getElementById('main-headline');
+           h.appendChild(l);
+         }
+         else if(i<6) {
+
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+
+             var l= document.createElement('div');
+              l.innerHTML=t;
+             var h=document.getElementById('more-headlines');
+             h.appendChild(l);
+         }
+         else if(i<10)
+         {
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+
+             var l= document.createElement('div');
+               l.innerHTML=t;
+             var h=document.getElementById('sidebars');
+             h.appendChild(l);
+         }
        }
-     })
+       })
  .catch((err)=> {console.error(err);})
 }
 //different channels
@@ -19,35 +50,65 @@ function myfun()
  for(let i=0;i<z;i++){
    m[i].addEventListener("click",function(){
 	   var search_val=document.getElementById("input").value;
-	   
+
 	   if(search_val.length >0)
-	   {  
+	   {
 		    document.getElementById("input").value=" ";
 	   }
 	    for(let k=0;k<8;k++)
        {
-		   
+
         if(document.getElementById("ch"+k).checked)
-        { 
+        {
          document.getElementById("ch"+k).checked=false;
 		 }
       }
 
-	   
+
    channels(this.id);
-    
+
    });
  }
 function channels(channelname)
 {
+
+  document.getElementById("more-headlines").innerHTML="";
+  document.getElementById("main-headline").innerHTML="";
+  document.getElementById("sidebars").innerHTML="";
   fetch(`https://newsapi.org/v2/top-headlines?sources=${channelname}&apiKey=94e25a4c66eb4ea791f3898bdce7bcea`)
   .then(data => data.json())
        .then(data =>{
-         for(let i=0;i<10;i++)
-         {document.getElementById("headline"+i).innerHTML=data.articles[i].title;
-         document.getElementById("li"+i).href=data.articles[i].url;
-       document.getElementById('image'+i).src =data.articles[i].urlToImage;
-      document.getElementById("de"+i).innerHTML=data.articles[i].description;
+         for(let i=0;i<data.articles.length;i++)
+         { if(i<3)
+         {
+         var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+         <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+           <p id="de">${data.articles[i].description}</p>`;
+         var l= document.createElement('div');
+           l.innerHTML=t;
+           var h=document.getElementById('main-headline');
+           h.appendChild(l);
+         }
+         else if(i<6) {
+
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+          var l= document.createElement('div');
+              l.innerHTML=t;
+             var h=document.getElementById('more-headlines');
+             h.appendChild(l);
+         }
+         else if(i<10)
+         {
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+         var l= document.createElement('div');
+               l.innerHTML=t;
+             var h=document.getElementById('sidebars');
+             h.appendChild(l);
+         }
     }})
   .catch((err)=> {console.error(err);})
 }
@@ -75,30 +136,63 @@ let x=document.getElementById("input").value;
          {
               alert("News Related to "+ x +" Not Available in "+search);
          }
-         else{
-        for(let i=0;i<10;i++)
-         {document.getElementById("headline"+i).innerHTML=data.articles[i].title;
-         document.getElementById("li"+i).href=data.articles[i].url;
-         document.getElementById('image'+i).src =data.articles[i].urlToImage;
-        document.getElementById("de"+i).innerHTML=data.articles[i].description;}
+    else{
+      document.getElementById("more-headlines").innerHTML="";
+      document.getElementById("main-headline").innerHTML="";
+      document.getElementById("sidebars").innerHTML="";
+        for(let i=0;i<data.articles.length;i++)
+    {
+        if(i<3)
+         {
+         var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+         <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+           <p id="de">${data.articles[i].description}</p>`;
+         var l= document.createElement('div');
+          l.innerHTML=t;
+           var h=document.getElementById('main-headline');
+           h.appendChild(l);
          }
+         else if(i<6) {
+
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+
+             var l= document.createElement('div');
+              l.innerHTML=t;
+             var h=document.getElementById('more-headlines');
+             h.appendChild(l);
+         }
+         else if(i<10)
+         {
+           var t=`<img id="image"  src="${data.articles[i].urlToImage}" alt="image not loaded" width="300" height="228"/>
+           <a href="${data.articles[i].url}" id="li"><h1 id="headline">${data.articles[i].title}</h1></a>
+             <p id="de">${data.articles[i].description}</p>`;
+
+          var l= document.createElement('div');
+               l.innerHTML=t;
+             var h=document.getElementById('sidebars');
+             h.appendChild(l);
+             }
+         }
+   }
 		 })
 
 		var search_val=document.getElementById("input").value;
-	   
+
 	   if(search_val.length >0)
-	   {  
+	   {
 		    document.getElementById("input").value=" ";
 	   }
 	    for(let k=0;k<8;k++)
        {
 		   var f=document.getElementById("ch"+k).checked;
         if(f)
-        { 
+        {
          document.getElementById("ch"+k).checked=false;
 		 }
       }
-	  
+
 }
 else
   {
